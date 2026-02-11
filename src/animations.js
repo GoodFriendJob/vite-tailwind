cf1cd7a656474dc809154fc7b545f8a35ff4bd16
Good Friend Job <145948066+GoodFriendJob@users.noreply.github.com>
2026-02-11 11:59:59 -0600

    no need

--- src/animations.js
+++ src/animations.js
@@ -1,6 +1,10 @@
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export function initAnimations() {
+  // Hero
   gsap.from(".hero-title", {
     y: 40,
     opacity: 0,
@@ -14,5 +18,20 @@ export function initAnimations() {
     duration: 0.9,
     delay: 0.15,
     ease: "power3.out",
+  });
+
+  // Section titles: fade in when in view
+  gsap.utils.toArray(".section-title").forEach((el) => {
+    gsap.from(el, {
+      scrollTrigger: {
+        trigger: el,
+        start: "top 85%",
+        toggleActions: "play none none none",
+      },
+      y: 24,
+      opacity: 0,
+      duration: 0.7,
+      ease: "power3.out",
+    });
   });
 }
